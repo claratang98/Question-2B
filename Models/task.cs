8f9b8c7a6713825cbe458961f3076a39784404c9
Ga Yi Clara Tang <tanggy@simtech.a-star.edu.sg>
2021-10-11 14:46:15 +0800

    first commit

---
+++ Models/task.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace producer.Models
+{
+public class task  
+  {  
+      //public string Greet { get; set; }
+      public string email {get; set; }
+      public string password {get; set; }
+      public string Task {get; set; }
+    }
+}  
